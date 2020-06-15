681eb9f69dbb7e36d5126dded2d6b6e797a5e370
marthaserpak <m.v.serpak@gmail.com>
2020-06-15 18:20:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/block9/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.block9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        String message = ((EditText) findViewById(R.id.editText_message))
+                .getText().toString();
+        Uri destination = Uri.parse("smsto:5554");
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, destination);
+        smsIntent.putExtra("sms_body", message);
+        startActivity(smsIntent);
+    }
+}
